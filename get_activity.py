5412de72c3321785e75b10db59ed85dcd25e8013
gest2356 <vojta.javurek@gmail.com>
2026-03-11 19:31:08 +0100

    update

--- get_activity.py
+++ get_activity.py
@@ -6,5 +6,21 @@ def get_activity(username_to_search):
 
    if res.status_code == 200:
       print("nice")
+      filtered_events = []
+      for event in events:
+         event_id = event['id']
+         event_type = event['type']
+         event_name = event['repo']['name']
+         event_created = event['created_at']
+         temp = {event_id, event_type, event_name, event_created}
+         filtered_events.append(temp)
+
+      for event in filtered_events:
+         created_at = event[3]
+
+         created_at_formated = created_at.strftime('%Y-%m-%d')
+         print(f"{event[1]} --- {event[4]} --- {created_at_formated}")
+
+
    else:
       print(f"{res.status_code}\n {username_to_search} not found.")
